a54f4f19f45c06143b6533b23f42a41addd6c867
Samuel Muli <samuelmuli95@gmail.com>
2024-02-26 21:34:12 +0300

    Create signup resource

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import request, session
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import create_access_token
+from werkzeug.security import generate_password_hash
 
 from config import app, db, api
 from models import User, IncidentReport, MediaAttachment
@@ -8,6 +10,34 @@ from models import User, IncidentReport, MediaAttachment
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+
+        if not username or not email or not password:
+            return {'message': 'Username, email and password are required'}, 400
+
+        try:
+            user = User(username=username, email=email)
+            user._password_hash = password
+            db.session.add(user)
+            db.session.commit()
+        except IntegrityError:
+            return {'message': 'User with that email already exists'}, 400
+        
+        access_token = create_access_token(identity=user.id)
+        return {
+            'access_token': access_token,
+            'id': user.id,
+            'username': user.username,
+            'email': user.email
+        }, 201
+
+api.add_resource(Signup, '/signup')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- config.py
+++ config.py
@@ -4,12 +4,16 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 # app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ajali:91015@localhost/ajali'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+app.config['JWT_SECRET_KEY'] = 'super-secret'
+
+jwt = JWTManager(app)
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
